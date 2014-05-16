794ece1b72ca94295227c9dc3199d1e0a34b60a0
Insality <insality@gmail.com>
2014-05-16 23:31:56 +0600

    First test implement.

--- test/GameTest.java
+++ test/GameTest.java
@@ -4,5 +4,10 @@ import junit.framework.TestCase;
 public class GameTest extends TestCase {
 	public void testGutterGame() throws Exception {
 		Game g = new Game();
+		
+		for (int i=0; i<20; i++){
+			g.roll(0);
+		}
+		assertEquals(0, g.score());
 	}
 }
